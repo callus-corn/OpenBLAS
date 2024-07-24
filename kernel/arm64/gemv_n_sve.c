c709b619cec5cf57950d86976582062435128ec5
callus-corn <mtfm_ymzk@yahoo.co.jp>
2024-07-24 16:54:39 +0900

    test

--- kernel/arm64/gemv_n_sve.c
+++ kernel/arm64/gemv_n_sve.c
@@ -37,13 +37,13 @@ USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 #define SV_COUNT svcntd
 #define SV_TYPE svfloat64_t
 #define SV_TRUE svptrue_b64
-#define SV_WHILE svwhilelt_b64_u64
+#define SV_WHILE svwhilelt_b64
 #define SV_DUP svdup_f64
 #else
 #define SV_COUNT svcntw
 #define SV_TYPE svfloat32_t
 #define SV_TRUE svptrue_b32
-#define SV_WHILE svwhilelt_b32_u32
+#define SV_WHILE svwhilelt_b32
 #define SV_DUP svdup_f32
 #endif
 
@@ -55,6 +55,9 @@ int CNAME(BLASLONG m, BLASLONG n, BLASLONG dummy1, FLOAT alpha, FLOAT *a, BLASLO
   FLOAT *a_ptr;
   FLOAT temp;
 
+  long left;
+  long right;
+
   ix = 0;
   a_ptr = a;
 
@@ -63,14 +66,14 @@ int CNAME(BLASLONG m, BLASLONG n, BLASLONG dummy1, FLOAT alpha, FLOAT *a, BLASLO
     for (j = 0; j < n; j++) {
       SV_TYPE temp_vec = SV_DUP(alpha * x[ix]);
       i = 0;
-      svbool_t pg = SV_WHILE(i, m);
+      svbool_t pg = SV_WHILE(left, right);
       while (svptest_any(SV_TRUE(), pg)) {
         SV_TYPE a_vec = svld1(pg, a_ptr + i);
         SV_TYPE y_vec = svld1(pg, y + i);
         y_vec = svmla_x(pg, y_vec, temp_vec, a_vec);
         svst1(pg, y + i, y_vec);
         i += sve_size;
-        pg = SV_WHILE(i, m);
+        pg = SV_WHILE(left, right);
       }
       a_ptr += lda;
       ix += inc_x;
